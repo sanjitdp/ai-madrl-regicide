a15fd87da7d4194de9d220d63ca7a37ad3ef4a0e
Sanjit Dandapanthula <sanjitd360@gmail.com>
2024-04-27 16:40:12 -0700

    fixed bugs with environment

--- src/game_env/env.py
+++ src/game_env/env.py
@@ -6,18 +6,18 @@ class RegicideEnv(gym.Env):
     def __init__(self):
         super().__init__()
 
-        self.action_space = Tuple(MultiBinary(7), MultiBinary(7))
+        self.action_space = Tuple([MultiBinary(7), MultiBinary(7)])
         self.observation_space = Dict(
             {
                 "curr_enemy": Discrete(12),
-                "enemies_left": Discrete(12, start=0),
-                "curr_suits_left": MultiDiscrete([5] * 3, start=[0] * 3),
-                "enemy_health": Discrete(21, start=0),
-                "enemy_attack": Discrete(21, start=0),
-                "num_discard": Discrete(53, start=0),
-                "num_tavern": Discrete(53, start=0),
-                "player_hand": MultiDiscrete([53] * 7, start=[0] * 7),
-                "ally_hand": MultiDiscrete([53] * 7, start=[0] * 7),
+                "enemies_left": Discrete(12),
+                "curr_suits_left": MultiDiscrete([5] * 3),
+                "enemy_health": Discrete(21),
+                "enemy_attack": Discrete(21),
+                "num_discard": Discrete(53),
+                "num_tavern": Discrete(53),
+                "player_hand": MultiDiscrete([53] * 7),
+                "ally_hand": MultiDiscrete([53] * 7),
             }
         )
 
@@ -29,5 +29,3 @@ class RegicideEnv(gym.Env):
 
     def render(self):
         raise NotImplementedError
-
-    _suit_to_idx = {"hearts": 0, "diamonds": 1, "clubs": 2, "spades": 3}
